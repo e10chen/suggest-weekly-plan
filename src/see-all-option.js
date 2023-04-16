2e838670029935da0e8b545cd750c22899d83637
Ethan Chen <ethanchen229@gmail.com>
2023-04-16 19:43:38 -0400

    made a detials and stateful to see if open is true or not

--- src/see-all-option.js
+++ src/see-all-option.js
@@ -3,6 +3,10 @@ import { LitElement, html, css } from "lit";
 class SeeAllOption extends LitElement {
   static properties = {
     header: { type: String },
+    opened: {
+      type: Boolean,
+      reflect: true,
+    },
   };
 
   static styles = css`
@@ -12,13 +16,49 @@ class SeeAllOption extends LitElement {
     }
 
     .space-between-heading-and-button {
-        margin-left: 20px;
+      margin-left: 20px;
+    }
+
+    details > summary {
+      list-style: none;
+      cursor: pointer;
+    }
+
+    .video-title {
+      margin: 10px;
     }
   `;
 
   constructor() {
     super();
     this.header = "My app";
+    this.opened = false;
+  }
+
+  toggleEvent(e) {
+    const state =
+      this.shadowRoot.querySelector("see-all-button").getAttribute("open") ===
+      ""
+        ? true
+        : false;
+    this.opened = state;
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === "opened") {
+        this.dispatchEvent(
+          new CustomEvent("open-changed", {
+            composed: true,
+            bubbles: true,
+            cacnelable: false,
+            detail: {
+              value: this[propName],
+            },
+          })
+        );
+      }
+    });
   }
 
   render() {
@@ -28,9 +68,20 @@ class SeeAllOption extends LitElement {
           <div class="title">9 videos (Total 55 min). 3 reading, 1 quiz</div>
         </div>
         <div class="space-between-heading-and-button">
-          <button class="see-all-button">
-            <div class="button-text">See all</div>
-          </button>
+          <details
+            class="see-all-button"
+            .open=${this.opened}
+            @toggle="${this.toggleEvent}"
+          >
+            <summary class="button-text">See all</summary>
+            <div class="list-of-video">
+              <div class="video-title">reeeeee</div>
+              <div class="video-title">reeeeee</div>
+              <div class="video-title">reeeeee</div>
+              <div class="video-title">reeeeee</div>
+              <div class="video-title">reeeeee</div>
+            </div>
+          </details>
         </div>
       </div>
     `;
