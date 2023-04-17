1f19b680e2e0feedbc1112690a4db69cbf627a90
Ethan Chen <ethanchen229@gmail.com>
2023-04-16 23:13:59 -0400

    created api see how it will go

---
+++ api/weekdescription.js
@@ -0,0 +1,21 @@
+export default async function handler(request, res) {
+
+  console.log(request.query.search);
+
+const weekdescription = [
+  {
+      "weeknumber": "1",
+      "timetocomplete": "4 hours to complete",
+      "headline": "Something idk",
+      "description": "In this module, you will learn what it means to be happy and why pursuing happiness is not a pointless endeavor. Dr. Santos addresses how our minds lie to us and how the science shows that our misconceptions about money, grades, and social media are holding us back from implementing the techniques studied in positive psychology.",
+      "videoreadingquiztitle": "9 videos (Total 55 min). 3 reading, 1 quiz"
+  },
+
+];
+res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+res.json(weekdescription);
+}
