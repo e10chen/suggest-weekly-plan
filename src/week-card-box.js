8997212af683e4876d5f8df5941b4ec96328c96c
Ethan Chen <ethanchen229@gmail.com>
2023-04-16 22:04:28 -0400

    deleting an element

--- src/week-card-box.js
+++ src/week-card-box.js
@@ -1,9 +1,13 @@
 import { LitElement, html, css } from "lit";
-import "./see-all-option.js"
+import "./see-all-option.js";
 
 class WeekCardBox extends LitElement {
   static properties = {
     header: { type: String },
+    opened: {
+      type: Boolean,
+      reflect: true,
+    },
   };
 
   static styles = css`
@@ -41,12 +45,56 @@ class WeekCardBox extends LitElement {
       padding: 25px;
     }
 
+    .video-reading-quiz-see-all {
+      display: flex;
+      padding: 10px;
+    }
+
+    .space-between-heading-and-button {
+      margin-left: 20px;
+    }
+
+    details > summary {
+      list-style: none;
+      cursor: pointer;
+    }
+
+    .video-title {
+      margin: 10px;
+    }
 
   `;
 
   constructor() {
     super();
     this.header = "My app";
+    this.opened = false;
+  }
+
+  toggleEvent(e) {
+    const state =
+      this.shadowRoot.querySelector("see-all-button").getAttribute("open") ===
+      ""
+        ? true
+        : false;
+    this.opened = state;
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === "opened") {
+        this.dispatchEvent(
+          new CustomEvent("open-changed", {
+            composed: true,
+            bubbles: true,
+            cacnelable: false,
+            detail: {
+              value: this[propName],
+            },
+          })
+        );
+      }
+    });
   }
 
   render() {
@@ -68,7 +116,29 @@ class WeekCardBox extends LitElement {
             back from implementing the techniques studied in positive
             psychology.
           </div>
-          <see-all-option></see-all-option>
+          <div class="video-reading-quiz-see-all">
+            <div class="video-reading-quiz-heading">
+              <div class="title">
+                9 videos (Total 55 min). 3 reading, 1 quiz
+              </div>
+            </div>
+            <div class="space-between-heading-and-button">
+              <details
+                class="see-all-button"
+                .open=${this.opened}
+                @toggle="${this.toggleEvent}"
+              >
+                <summary class="button-text">See all</summary>
+                <div class="list-of-video">
+                  <div class="video-title">reeeeee</div>
+                  <div class="video-title">reeeeee</div>
+                  <div class="video-title">reeeeee</div>
+                  <div class="video-title">reeeeee</div>
+                  <div class="video-title">reeeeee</div>
+                </div>
+              </details>
+            </div>
+          </div>
         </div>
       </div>
     `;
